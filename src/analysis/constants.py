f47c4cec474cfbeaa19bdea29bd233e9f4ebcf5e
Carlo Musolino <carlo.musolino@aei.mpg.de>
2026-03-26 11:44:21 +0100

    Add eos utilities

--- src/analysis/constants.py
+++ src/analysis/constants.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # Units are CGS unless specified 
 # speed of light
-clight = 29979245800
+c_cgs = 29979245800
 # Planck constant eV s 
 h_eVs = 6.582119569e-16 
 # Boltzmann constant 
@@ -29,13 +29,24 @@ me_MeV = 0.51099895069
 me_KeV = 510.99895069
 # Magnetic
 mu0 = 1.2566370614359173e-6 # Vacuum permeability
-eps0 = 1.0/(mu0*clight**2) 
+eps0 = 1.0/(mu0*c_cgs**2) 
+# Proton mass 
+mp_MeV = 938.27208943 
+mn_MeV = 939.56542194
 
+# SI units 
+c_si = c_cgs * 1e-2
+# Mass 
+Msun_si = Msun_cgs * 1e-3 
+# G_si 
+G_si = G_cgs * 1e-3 
 
 # conversions 
 
 # Length
 cm_to_m = 1e2 
+fm_to_m = 1e-15
+fm_to_cm = 1e-13 
 m_to_cm = 1e-2 
 cm_to_km = 1e5 
 km_to_cm = 1e-5
@@ -48,18 +59,18 @@ km_to_pc = 1/pc_to_km
 angstrom_to_nm = 0.1 
 nm_to_angstrom = 10 
 # Assuming c = G = Msun = 1  
-Msun_to_cm = G_cgs * Msun_cgs / clight**2
+Msun_to_cm = G_cgs * Msun_cgs / c_cgs**2
 cm_to_Msun = 1./Msun_to_cm 
 Msun_to_pc = Msun_to_cm * cm_to_km * km_to_pc
 # B field
-CU_to_Gauss = (1.0/Msun_to_cm/np.sqrt(eps0*G_cgs/(clight**2))) / 1e09 ;
+CU_to_Gauss = (1.0/Msun_to_cm/np.sqrt(eps0*G_cgs/(c_cgs**2))) / 1e09 ;
 
 # Time 
 hour_to_s = 60 * 60 
 day_to_s = 24*hour_to_s 
 year_to_s = 365 * day_to_s 
 s_to_year = 1/year_to_s
-Msun_to_s = Msun_to_cm / clight 
+Msun_to_s = Msun_to_cm / c_cgs 
 s_to_Msun = 1./Msun_to_s
 ms_to_Msun = s_to_Msun * 1e-3 
 Msun_to_ms = 1e03 * Msun_to_s
@@ -75,10 +86,12 @@ eV_to_erg = e_charge * J_to_erg
 erg_to_eV = 1/eV_to_erg
 erg_to_keV = erg_to_eV * 1e-3 
 erg_to_MeV = erg_to_eV * 1e-6
+MeV_to_erg = 1/erg_to_MeV
 
 # Mass 
-eV_to_g = eV_to_erg/clight**2
+eV_to_g = eV_to_erg/c_cgs**2
 MeV_to_g = eV_to_g * 1e6 
+MeV_to_kg = MeV_to_g * 1e-3
 
 # Boltzmann constant in CGS 
 k_cgs = k_evKm1 * eV_to_erg
---
+++ src/eos/units_system.py
@@ -0,0 +1,37 @@
+import numpy as np 
+from analysis.constants import * 
+
+
+class unit_system:
+
+    def __init__(self, mass, length, time):
+        self.length = float(length)
+        self.time   = float(time)
+        self.mass   = float(mass)
+
+        self.velocity = self.length / self.time 
+        self.acceleration = self.velocity / self.time 
+        self.force = self.acceleration * self.mass 
+        self.surface = self.length**2
+        self.volume = self.length**3 
+
+        self.pressure = self.force / self.surface
+        self.dens   = self.mass / self.volume
+        self.energy = self.force * self.length 
+        self.edens  = self.energy / self.volume 
+
+
+    def __truediv__(self, other):
+        return unit_system(self.mass/other.mass, self.length/other.length, self.time/other.time)
+    
+# SI, the base unit system
+SI_UNIT_SYSTEM = unit_system(1,1,1)
+# CGS 
+CGS_UNIT_SYSTEM = unit_system(1e-3,1e-2,1)
+# c = G = Msun = 1 
+GEOM_UNIT_SYSTEM = unit_system(Msun_si, G_si * Msun_si / c_si**2, G_si * Msun_si / c_si**3 )
+# Compose units 
+COMPOSE_UNIT_SYSTEM = unit_system(MeV_to_kg, fm_to_m, fm_to_m/c_si)
+
+
+
